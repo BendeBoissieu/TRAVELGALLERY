9a6117fdee42851c1cd773dc0c56f47bf40813ea
benjamin de boissieu <bendeb@hotmail.com>
2019-06-12 22:44:01 +0200

    image meta for google and other social networks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
   def getCategoryNav
     @categoryNav = Category.all
   end
+
+  def default_url_options
+  { host: ENV["https://mytravelgallery.herokuapp.com/"] || "localhost:3000" }
+  end
+
 end
